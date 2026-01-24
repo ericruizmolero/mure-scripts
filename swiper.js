838c4497f10e2157697df72bd0e540f7cf1884a0
ericruizmolero <154838631+ericruizmolero@users.noreply.github.com>
2026-01-24 21:50:33 +0100

    Update swiper.js

--- swiper.js
+++ swiper.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   const solutionSwiper = new Swiper('.swiper.is-work', {
-    slidesPerView: '1',
+    slidesPerView: 'auto',
     speed: 1000,         
     spaceBetween: 0,
     grabCursor: true,
@@ -8,17 +8,5 @@ document.addEventListener('DOMContentLoaded', function () {
     navigation: {
       nextEl: ".work-next",
       prevEl: ".work-prev",
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      1250: {
-        slidesPerView: '1', // o el número que necesites
-      }
     }
-  });
 });
